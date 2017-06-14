e127ae53017881554ba42f4730a8c88d21099fa0
fuookami <bagawolf2014@yahoo.co.jp>
2017-06-14 09:26:24 +0800

    重命名项目

--- 课设/DataBaseSystemCurriculumDesign/DataBaseSystemCurriculumDesign/main.cpp
+++ 课设/DataBaseSystemCurriculumDesign/DataBaseSystemCurriculumDesign/main.cpp
@@ -1,7 +1,10 @@
+#include "databasesystemcurriculumdesign.h"
 #include <QtWidgets/QApplication>
 
 int main(int argc, char *argv[])
 {
 	QApplication a(argc, argv);
+	DataBaseSystemCurriculumDesign w;
+	w.show();
 	return a.exec();
 }
