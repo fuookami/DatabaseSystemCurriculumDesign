fa286e35d6b1b9d15eff0174c4ee6abed8e12361
fuookami <bagawolf2014@yahoo.co.jp>
2017-06-16 03:47:02 +0800

    重定向qt库，编译qoci库，数据注入模块连接数据库成功

--- 课设/DataBaseSystemCurriculumDesign/DataGenerater/main.cpp
+++ 课设/DataBaseSystemCurriculumDesign/DataGenerater/main.cpp
@@ -1,7 +1,21 @@
 #include <QtWidgets/QApplication>
+#include <QtSql/QSqlDatabase>
+#include <QtSql/QSqlQuery>
+#include <QtCore/QDebug>
 
 int main(int argc, char *argv[])
 {
 	QApplication a(argc, argv);
+	QSqlDatabase db(QSqlDatabase::addDatabase("QOCI"));
+	db.setPort(1521);
+	db.setHostName("127.0.0.1");
+	db.setDatabaseName("fuookamiDBSD");
+	db.setUserName("fuookami");
+	db.setPassword("a08040228a");
+	if (db.open())
+	{
+		qDebug() << "ok";
+	}
+
 	return a.exec();
 }
