9f9849df92793850eef6974c52bddc31e1d05c3c
fuookami <bagawolf2014@yahoo.co.jp>
2017-06-16 17:02:36 +0800

    搭建数据生成框架

--- 课设/DataBaseSystemCurriculumDesign/DataGenerater/main.cpp
+++ 课设/DataBaseSystemCurriculumDesign/DataGenerater/main.cpp
@@ -3,36 +3,63 @@
 #include <QtSql/QSqlQuery>
 #include <QtSql/QSqlError>
 #include <QtCore/QDebug>
+#include <QtCore/QString>
+#include <QtCore/QStringList>
+#include <memory>
+
+QStringList familyNames;
+QStringList names;
+QStringList mobileMacs;
+QStringList telephoneMacs;
+
+QSqlDatabase *createConnectTo(const QString &dataType, const QString &host, const unsigned int port, const QString &dbName, const QString &user, const QString &password)
+{
+	QSqlDatabase *db(&QSqlDatabase::addDatabase(dataType));
+	db->setHostName(host);
+	db->setPort(port);
+	db->setDatabaseName(dbName);
+	db->setUserName(user);
+	db->setPassword(password);
+
+	return db;
+}
+
+void loadNames()
+{
+
+}
+
+void loadMobileMacs()
+{
+
+}
+
+void loadTelephoneMacs()
+{
+
+}
+
+void loadData()
+{
+	loadNames();
+	loadMobileMacs();
+	loadTelephoneMacs();
+}
 
 int main(int argc, char *argv[])
 {
 	QApplication a(argc, argv);
-	QSqlDatabase db(QSqlDatabase::addDatabase("QOCI"));
-	db.setPort(1521);
-	db.setHostName("127.0.0.1");
-	db.setDatabaseName("fuookamiDBSD");
-	db.setUserName("fuookami");
-	db.setPassword("a08040228a");
-	if (db.open())
-	{
-		qDebug() << "ok";
-		QSqlQuery query;
-		query.exec("SELECT SNO, SNAME FROM FUOOKAMI.S");
 
-		if (!query.isActive())
-		{
-			qDebug() << query.lastError().text();
-		}
-		else 
-		{
-			while (query.next())
-			{
-				QString sno = query.value(0).toString();
-				QString sname = query.value(1).toString();
-				qDebug() << sno << ' ' << sname << '\n';
-			}
-		}
+	loadData();
+
+	QSqlDatabase *db(createConnectTo("QOCI", "127.0.0.1", 1521, "fuookamiDBSD", "fuookami", "a08040228a"));
+	if (!db->open())
+	{
+		qDebug() << "DB connection wrong: " << db->lastError().text();
+		delete db;
+		return 1;
 	}
 
+	delete db;
 	return a.exec();
 }
